1baf12d4994031a54c97ba805a567c3645365b57
unknown <lucianproseg@gmail.com>
2022-01-07 15:53:39 -0300

    Projeto Contador

---
+++ P.Contador/script.js
@@ -0,0 +1,21 @@
+let mudar = document.getElementById("title");
+let number =0
+
+
+function encrementar() {
+
+    number++
+    mudar.innerText = number
+
+
+
+}
+
+function decrementar() {
+
+    number--
+    mudar.innerText = number
+
+
+
+}
